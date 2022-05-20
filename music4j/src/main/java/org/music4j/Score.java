d3e76a744d27aff0fb1ca4d48d36fd42da7225a8
Andreas <andreas.goss1994@gmail.com>
2022-05-20 21:28:13 +0200

    Add Simple implementation and static factory for Score

--- music4j/src/main/java/org/music4j/Score.java
+++ music4j/src/main/java/org/music4j/Score.java
@@ -1,9 +1,20 @@
 package org.music4j;
 
+import org.music4j.simple.ArrayScore;
+
 /**
  * A Score is an ordered list of Parts. This interface defines a root object for
  * all musical objects in the api.
  */
 public interface Score {
+
+    /**
+     * Returns an empty Score.
+     *
+     * @return an empty Score.
+     */
+    static Score of() {
+        return new ArrayScore();
+    }
 
 }
---
+++ music4j/src/main/java/org/music4j/simple/ArrayScore.java
@@ -0,0 +1,10 @@
+package org.music4j.simple;
+
+import org.music4j.Score;
+
+/**
+ * Simple implementation of a Score based on an ArrayList.
+ */
+public class ArrayScore implements Score {
+
+}
