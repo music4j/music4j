157d0dd4e7efeb4c86e8e80bd25c06fdcba3ba04
Andreas <andreas.goss1994@gmail.com>
2022-05-25 20:48:13 +0200

    Fix minor bug for Midi Translator

--- music4j/src/main/java/org/music4j/midi/MidiTranslator.java
+++ music4j/src/main/java/org/music4j/midi/MidiTranslator.java
@@ -19,52 +19,54 @@ import org.music4j.Staff;
 import org.music4j.Voice;
 
 /**
- * Class that translates a {@link Score} to a {@link Sequence} 
+ * Class that translates a {@link Score} to a {@link Sequence}
  */
 public class MidiTranslator {
-	
+
 	/**
-	 * The resolution of the score ist the least common multiple of all parts and voices
+	 * The resolution of the score ist the least common multiple of all parts and
+	 * voices
 	 */
-    private final int resolution;
-    
-    /**
-     * The translated Sequence
-     */
-    private final Sequence seq;
-    
-    /**
-     * Current Track in which midi events are added.
-     */
-    private Track track;
-    
-    /**
-     * The the current counter position of the sequence.
-     */
-    private BarTime counter;
-    
-    /**
-     * Constructor translates Score to {@link Sequence}
-     * @param pitch
-     */
-    public MidiTranslator(Score score) {
-    	resolution = calculateResolution(score);
-    	counter = BarTime.ZERO;
+	private final int resolution;
+
+	/**
+	 * The translated Sequence
+	 */
+	private final Sequence seq;
+
+	/**
+	 * Current Track in which midi events are added.
+	 */
+	private Track track;
+
+	/**
+	 * The the current counter position of the sequence.
+	 */
+	private BarTime counter;
+
+	/**
+	 * Constructor translates Score to {@link Sequence}
+	 * 
+	 * @param pitch
+	 */
+	public MidiTranslator(Score score) {
+		resolution = calculateResolution(score);
+		counter = BarTime.ZERO;
 		try {
 			seq = new Sequence(Sequence.PPQ, resolution);
 			translate(score);
 		} catch (InvalidMidiDataException e) {
-			//Should not happen
+			// Should not happen
 			throw new RuntimeException(e);
 		}
-    }
-    
-    public Sequence getSequence() {
-    	return seq;
-    }
+	}
+
+	public Sequence getSequence() {
+		return seq;
+	}
 
 	private void translate(Score score) {
-		for(Part part : score) {
+		for (Part part : score) {
 			counter = BarTime.ZERO;
 			track = seq.createTrack();
 			translate(part);
@@ -72,27 +74,27 @@ public class MidiTranslator {
 	}
 
 	private void translate(Part part) {
-		for(Staff staff : part) {
+		for (Staff staff : part) {
 			translate(staff);
 		}
 	}
 
 	private void translate(Staff staff) {
-		for(Bar bar : staff) {
+		for (Bar bar : staff) {
 			translate(bar);
 		}
 	}
 
 	private void translate(Bar bar) {
-		for(Voice voice : bar) {
+		for (Voice voice : bar) {
 			translate(voice);
 		}
-		//After the bar is translated increase the counter.
+		// After the bar is translated increase the counter.
 		counter = counter.plus(bar.length());
 	}
 
 	private void translate(Voice voice) {
-		for(Entry<BarTime, Note> e : voice.entrySet()) {
+		for (Entry<BarTime, Note> e : voice.entrySet()) {
 			addNoteToTrack(counter.plus(e.getKey()), e.getValue());
 		}
 	}
@@ -104,23 +106,23 @@ public class MidiTranslator {
 			checkDivisibility(noteEnd);
 			int timeStart = time.getNumerator() * (resolution / time.getDenominator());
 			int timeEnd = noteEnd.getNumerator() * (resolution / noteEnd.getDenominator());
-			for(Pitch p: note) {
-				//Switch note on
+			for (Pitch p : note) {
+				// Switch note on
 				MidiMessage noteOn = new ShortMessage(ShortMessage.NOTE_ON, 0, p.asInt(), 64);
 				track.add(new MidiEvent(noteOn, timeStart));
-				//Switch note off
+				// Switch note off
 				MidiMessage noteOff = new ShortMessage(ShortMessage.NOTE_OFF, 0, p.asInt(), 0);
 				track.add(new MidiEvent(noteOff, timeEnd));
 			}
-			
+
 		} catch (InvalidMidiDataException e) {
-			//Cannot occur
+			// Cannot occur
 			throw new RuntimeException(e);
 		}
 	}
 
 	private void checkDivisibility(BarTime time) {
-		if(resolution % time.getDenominator() != 0) {
+		if (resolution % time.getDenominator() != 0) {
 			throw new RuntimeException("The resolution is not divisible by the denominator");
 		}
 	}
@@ -132,19 +134,21 @@ public class MidiTranslator {
 	private static int calculateResolution(Part part) {
 		return part.stream().map(MidiTranslator::calculateResolution).reduce(1, BarTime::leastCommonMultiple);
 	}
-	
+
 	private static int calculateResolution(Staff staff) {
 		return staff.stream().map(MidiTranslator::calculateResolution).reduce(1, BarTime::leastCommonMultiple);
 	}
-	
+
 	private static int calculateResolution(Bar bar) {
 		return bar.stream().map(MidiTranslator::calculateResolution).reduce(1, BarTime::leastCommonMultiple);
 	}
-	
+
 	private static int calculateResolution(Voice voice) {
-		return voice.keySet().stream().map(BarTime::getDenominator).reduce(1, BarTime::leastCommonMultiple);
+		int resolutionKeys = voice.keySet().stream().map(BarTime::getDenominator).reduce(1,
+				BarTime::leastCommonMultiple);
+		int resolutionValues = voice.values().stream().map(Note::getDuration).map(BarTime::getDenominator).reduce(1,
+				BarTime::leastCommonMultiple);
+		return BarTime.leastCommonMultiple(resolutionKeys, resolutionValues);
 	}
-	
-	
-	
+
 }
