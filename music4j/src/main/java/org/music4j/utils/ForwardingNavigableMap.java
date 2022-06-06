37ca5291514a3f585a0b1bc5ef96a7d6819b3aa5
Andreas <andreas.goss1994@gmail.com>
2022-06-06 13:06:37 +0200

    Add null checks for Forwarding Navigable Map

--- music4j/src/main/java/org/music4j/utils/ForwardingNavigableMap.java
+++ music4j/src/main/java/org/music4j/utils/ForwardingNavigableMap.java
@@ -4,6 +4,7 @@ import java.util.AbstractMap;
 import java.util.Comparator;
 import java.util.NavigableMap;
 import java.util.NavigableSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.SortedMap;
 
@@ -12,11 +13,14 @@ public class ForwardingNavigableMap<K,V> extends AbstractMap<K,V> implements Nav
     private final NavigableMap<K, V> map;
 
     public ForwardingNavigableMap(NavigableMap<K, V> map) {
+        Objects.requireNonNull(map);
         this.map = map;
     }
 
     @Override
     public V put(K key, V value) {
+        Objects.requireNonNull(key);
+        Objects.requireNonNull(value);
         return map.put(key, value);
     }
 
@@ -47,41 +51,49 @@ public class ForwardingNavigableMap<K,V> extends AbstractMap<K,V> implements Nav
 
     @Override
     public Entry<K, V> lowerEntry(K key) {
+        Objects.requireNonNull(key);
         return map.lowerEntry(key);
     }
 
     @Override
     public K lowerKey(K key) {
+        Objects.requireNonNull(key);
         return map.lowerKey(key);
     }
 
     @Override
     public Entry<K, V> floorEntry(K key) {
+        Objects.requireNonNull(key);
         return map.floorEntry(key);
     }
 
     @Override
     public K floorKey(K key) {
+        Objects.requireNonNull(key);
         return map.floorKey(key);
     }
 
     @Override
     public Entry<K, V> ceilingEntry(K key) {
+        Objects.requireNonNull(key);
         return map.ceilingEntry(key);
     }
 
     @Override
     public K ceilingKey(K key) {
+        Objects.requireNonNull(key);
         return map.ceilingKey(key);
     }
 
     @Override
     public Entry<K, V> higherEntry(K key) {
+        Objects.requireNonNull(key);
         return map.higherEntry(key);
     }
 
     @Override
     public K higherKey(K key) {
+        Objects.requireNonNull(key);
         return map.higherKey(key);
     }
 
@@ -112,31 +124,39 @@ public class ForwardingNavigableMap<K,V> extends AbstractMap<K,V> implements Nav
 
     @Override
     public NavigableMap<K, V> subMap(K fromKey, boolean fromInclusive, K toKey, boolean toInclusive) {
+        Objects.requireNonNull(fromKey);
+        Objects.requireNonNull(toKey);
         return map.subMap(fromKey, fromInclusive, toKey, toInclusive);
     }
 
     @Override
     public NavigableMap<K, V> headMap(K toKey, boolean inclusive) {
+        Objects.requireNonNull(toKey);
         return map.headMap(toKey, inclusive);
     }
 
     @Override
     public NavigableMap<K, V> tailMap(K fromKey, boolean inclusive) {
+        Objects.requireNonNull(fromKey);
         return map.tailMap(fromKey, inclusive);
     }
 
     @Override
     public SortedMap<K, V> subMap(K fromKey, K toKey) {
+        Objects.requireNonNull(fromKey);
+        Objects.requireNonNull(toKey);
         return map.subMap(fromKey, toKey);
     }
 
     @Override
     public SortedMap<K, V> headMap(K toKey) {
+        Objects.requireNonNull(toKey);
         return map.headMap(toKey);
     }
 
     @Override
     public SortedMap<K, V> tailMap(K fromKey) {
+        Objects.requireNonNull(fromKey);
         return map.tailMap(fromKey);
     }
 
